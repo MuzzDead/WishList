4ea6a3d80b518f1adaddfb75f0ab0caecec4084a
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2024-09-10 21:41:33 +0300

    Implement Wish Service

--- WishList/Services/IWishService.cs
+++ WishList/Services/IWishService.cs
@@ -1,5 +1,13 @@
-﻿namespace WishList.Services;
+﻿using WishList.Models;
+
+namespace WishList.Services;
 
 public interface IWishService
 {
+	Task<ICollection<Wish>> GetWishes();
+	Task<Wish> GetWishById(Guid id);
+
+	Task AddWish(Wish wish);
+	Task DeleteWish(Guid id);
+	Task UpdateWish(Guid id,Wish wish);
 }
---
+++ WishList/Services/WishService.cs
@@ -0,0 +1,38 @@
+﻿using WishList.Models;
+using WishList.Repositories;
+
+namespace WishList.Services;
+
+public class WishService : IWishService
+{
+	private readonly IWishRepository _wishRepository;
+    public WishService(IWishRepository wishRepository)
+    {
+        _wishRepository = wishRepository;
+    }
+
+    public async Task AddWish(Wish wish)
+	{
+		await _wishRepository.Add(wish);
+	}
+
+	public async Task DeleteWish(Guid id)
+	{
+		await _wishRepository.Remove(id);
+	}
+
+	public async Task<Wish> GetWishById(Guid id)
+	{
+		return await _wishRepository.GetById(id);
+	}
+
+	public async Task<ICollection<Wish>> GetWishes()
+	{
+		return await _wishRepository.GetAll();
+	}
+
+	public async Task UpdateWish(Guid id, Wish wish)
+	{
+		await _wishRepository.Update(id, wish);
+	}
+}
